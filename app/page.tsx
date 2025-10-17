54f6a4f6837d219a8a9c85727a7a307de63a505d
KUA-Rantepao <rantepao.kua@gmail.com>
2025-10-17 21:00:15 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 
 import Image from 'next/image';
 import Link from 'next/link';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 // Komponen Jadwal Shalat (Tanpa Hijriyah)
 function WidgetJadwalShalat() {
